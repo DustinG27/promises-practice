b4fdaa8b0fb65c4f50b46cc92c1f16699e2646eb
dustin george <DustinG27@gmail.com>
2021-08-18 20:00:05 -0400

    messed around with async functions

--- app.js
+++ app.js
@@ -35,8 +35,28 @@
         })
     }
 
-    delayedColorChange("red", 1000)
-        .then(() => delayedColorChange("green", 1000))
-        .then(() => delayedColorChange("blue", 1000))
-        .then(() => delayedColorChange("purple", 1000))
-        .then(() => delayedColorChange("grey", 1000))
+    // delayedColorChange("red", 1000)
+    //     .then(() => delayedColorChange("green", 1000))
+    //     .then(() => delayedColorChange("blue", 1000))
+    //     .then(() => delayedColorChange("purple", 1000))
+    //     .then(() => delayedColorChange("grey", 1000))
+
+    //async function of colors
+    async function rainbow() {
+        await delayedColorChange('red', 1000)
+        await delayedColorChange('orange', 1000)
+        await delayedColorChange('yellow', 1000)
+        await delayedColorChange('green', 1000)
+        await delayedColorChange('blue', 1000)
+        await delayedColorChange('indigo', 1000)
+        await delayedColorChange('violet', 1000)
+    }
+
+    async function awaitRainbow() {
+        await rainbow()
+            .then(() => {
+                delayedColorChange('black', 1000)
+            })
+    }
+
+    awaitRainbow();
